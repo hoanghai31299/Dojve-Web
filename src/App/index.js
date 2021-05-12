e395bebeaaaf0328d8a61913d3bf716434c91d50
HoangHai <hoanghai31299@gmail.com>
2021-05-12 15:58:25 +0700

    set cookie

--- src/App/index.js
+++ src/App/index.js
@@ -12,6 +12,7 @@ import { useDispatch } from "react-redux";
 import axios from "./utils/axios";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Cookies from "universal-cookie";
 function HomePage() {
   const dispatch = useDispatch();
   const [redirect, setRedirect] = useState(false);
@@ -24,6 +25,9 @@ function HomePage() {
         let user = data.user;
         user.token = data.token;
         dispatch(setUser(user));
+        const cookies = new Cookies();
+        cookies.set("token", data.token);
+        cookies.set("refreshToken", data.refreshToken);
         dispatch(connectSocket(data.token));
       } else {
         setRedirect(true);
